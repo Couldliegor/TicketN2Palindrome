13381e4d408574dbe87939afaf68ae49ced12d06
egorcouldlie <couldliegor@gmail.com>
2022-12-23 14:40:12 +0300

    Palindrome ready

---
+++ src/Palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome {
+     public static boolean isPalindrome(String word) {
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() -i -1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
